ae2d9a32d7273fb8477bf3ecc2a369affe1601e4
ravi verma <ravivermaj488@gmail.com>
2020-03-23 12:05:21 +0530

    Update

---
+++ Duplicate/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Duplicate
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textTotalFiles = new System.Windows.Forms.TextBox();
+            this.totalFiles = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listViewScanned = new System.Windows.Forms.ListView();
+            this.imageListScanned = new System.Windows.Forms.ImageList(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelStartStop = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.textBoxGroup = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.linkLabelPrj = new System.Windows.Forms.LinkLabel();
+            this.labelPrj = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.textTotalFiles);
+            this.groupBox1.Controls.Add(this.totalFiles);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.listView);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 277);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File";
+            // 
+            // textTotalFiles
+            // 
+            this.textTotalFiles.Location = new System.Drawing.Point(84, 238);
+            this.textTotalFiles.Name = "textTotalFiles";
+            this.textTotalFiles.ReadOnly = true;
+            this.textTotalFiles.Size = new System.Drawing.Size(33, 20);
+            this.textTotalFiles.TabIndex = 4;
+            this.textTotalFiles.Text = "0";
+            this.textTotalFiles.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // totalFiles
+            // 
+            this.totalFiles.AutoSize = true;
+            this.totalFiles.Location = new System.Drawing.Point(17, 241);
+            this.totalFiles.Name = "totalFiles";
+            this.totalFiles.Size = new System.Drawing.Size(61, 13);
+            this.totalFiles.TabIndex = 3;
+            this.totalFiles.Text = "Total Files :";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(87, 22);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(209, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(6, 48);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(290, 173);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(32, 32);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(6, 19);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox2.Controls.Add(this.listViewScanned);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox2.Location = new System.Drawing.Point(325, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(540, 409);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Processed Files";
+            // 
+            // listViewScanned
+            // 
+            this.listViewScanned.AllowColumnReorder = true;
+            this.listViewScanned.CheckBoxes = true;
+            this.listViewScanned.GridLines = true;
+            this.listViewScanned.HideSelection = false;
+            this.listViewScanned.LargeImageList = this.imageListScanned;
+            this.listViewScanned.Location = new System.Drawing.Point(6, 19);
+            this.listViewScanned.Name = "listViewScanned";
+            this.listViewScanned.Size = new System.Drawing.Size(528, 384);
+            this.listViewScanned.TabIndex = 0;
+            this.listViewScanned.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageListScanned
+            // 
+            this.imageListScanned.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListScanned.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageListScanned.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 445);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(853, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelStartStop);
+            this.groupBox3.Controls.Add(this.btnStop);
+            this.groupBox3.Controls.Add(this.textBoxStatus);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.textBoxGroup);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.btnStart);
+            this.groupBox3.Location = new System.Drawing.Point(12, 309);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(307, 126);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actions";
+            // 
+            // labelStartStop
+            // 
+            this.labelStartStop.AutoSize = true;
+            this.labelStartStop.Location = new System.Drawing.Point(6, 25);
+            this.labelStartStop.Name = "labelStartStop";
+            this.labelStartStop.Size = new System.Drawing.Size(122, 13);
+            this.labelStartStop.TabIndex = 7;
+            this.labelStartStop.Text = "Scan Duplicate Photos :";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(180, 19);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(51, 24);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(58, 90);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(116, 20);
+            this.textBoxStatus.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Status :";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(128, 53);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(116, 25);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Remove Duplicates";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // textBoxGroup
+            // 
+            this.textBoxGroup.Location = new System.Drawing.Point(84, 56);
+            this.textBoxGroup.Name = "textBoxGroup";
+            this.textBoxGroup.ReadOnly = true;
+            this.textBoxGroup.Size = new System.Drawing.Size(32, 20);
+            this.textBoxGroup.TabIndex = 2;
+            this.textBoxGroup.Text = "0";
+            this.textBoxGroup.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Groups :";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(128, 19);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(46, 25);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(15, 10);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(66, 13);
+            this.labelVersion.TabIndex = 4;
+            this.labelVersion.Text = "Version : 1.0";
+            // 
+            // linkLabelPrj
+            // 
+            this.linkLabelPrj.AutoSize = true;
+            this.linkLabelPrj.Location = new System.Drawing.Point(582, 9);
+            this.linkLabelPrj.Name = "linkLabelPrj";
+            this.linkLabelPrj.Size = new System.Drawing.Size(283, 13);
+            this.linkLabelPrj.TabIndex = 5;
+            this.linkLabelPrj.TabStop = true;
+            this.linkLabelPrj.Text = "https://github.com/raviverma2791747/Duplicate-Phoaster";
+            this.linkLabelPrj.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelPrj_LinkClicked);
+            // 
+            // labelPrj
+            // 
+            this.labelPrj.AutoSize = true;
+            this.labelPrj.Location = new System.Drawing.Point(501, 9);
+            this.labelPrj.Name = "labelPrj";
+            this.labelPrj.Size = new System.Drawing.Size(75, 13);
+            this.labelPrj.TabIndex = 6;
+            this.labelPrj.Text = "Source Code :";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 472);
+            this.splitter1.TabIndex = 7;
+            this.splitter1.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(869, 472);
+            this.Controls.Add(this.labelPrj);
+            this.Controls.Add(this.linkLabelPrj);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.splitter1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "Duplicate Phoaster";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listViewScanned;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textTotalFiles;
+        private System.Windows.Forms.Label totalFiles;
+        private System.Windows.Forms.ImageList imageListScanned;
+        private System.Windows.Forms.TextBox textBoxGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.LinkLabel linkLabelPrj;
+        private System.Windows.Forms.Label labelPrj;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labelStartStop;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
---
+++ Duplicate/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Duplicate
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        List<string> listFiles = new List<string>();
+        int groups = 0; //Counts total groups of duplicate and single  photos
+        bool operation = true; // state of operation
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            listFiles.Clear();
+            listView.Items.Clear();
+            groups = 0;
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() {Description = "Select Your Path."})
+            {
+                if(fbd.ShowDialog() == DialogResult.OK)
+                {
+                    string extn;
+                    txtPath.Text = fbd.SelectedPath;
+                    foreach(string item in System.IO.Directory.GetFiles(fbd.SelectedPath))
+                    {
+                        extn = Path.GetExtension(item);
+                        if (extn == ".PNG" || extn == ".JPEG" || extn == ".JPG" || extn == ".BMP")
+                        {
+                            imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(item));
+                            System.IO.FileInfo fi = new System.IO.FileInfo(item);
+                            listFiles.Add(fi.FullName);
+                            listView.Items.Add(fi.Name, imageList.Images.Count - 1);
+                        }
+                    }
+                    textTotalFiles.Text = Convert.ToString(imageList.Images.Count);
+                }
+            }
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listView.FocusedItem != null)
+            {
+                System.Diagnostics.Process.Start(listFiles[listView.FocusedItem.Index]);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Process of Duplicate file scanning takes place here
+            int cnt = 0;    //index for listViewScanned 
+            List<bool> visited = new List<bool>(); //saves the info already grouped photos
+            visited.Clear(); //remove garbage value
+           for(int i=0;i<listFiles.Count;i++)  //initialize it with true values meaning not grouped is true
+            {
+                visited.Add(false);
+            }
+            listViewScanned.Items.Clear();  //Clear garbage value if any
+            progressBar.Minimum = 0;        //initialize progresbar minimum value
+            progressBar.Maximum = listFiles.Count;    //initialize progress bar maximum value
+            progressBar.Value =  0;                  //starting value of progress bar
+            for (int i = 0; i < listFiles.Count; i++)     
+            {
+                progressBar.Value = i;         //updating  progressbar value with progress in task
+                if (visited[i] == false)  //checking if already visited 
+                {
+                    System.IO.FileInfo fi = new System.IO.FileInfo(listFiles[i]);
+                    for (int j = 0 ; j < listFiles.Count; j++)
+                    {
+                        System.IO.FileInfo fi2 = new System.IO.FileInfo(listFiles[j]);
+                        if (fi.Length == fi2.Length && fi.Extension == fi2.Extension)
+                        {
+                            
+                           if (!visited[i])
+                            {
+                                cnt += 1;  //updating index of listViewScanned.Items
+                                imageListScanned.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(listFiles[i]));
+                                listViewScanned.Items.Add(fi.Name, cnt);
+                                visited[i] = true;
+                                 groups += 1;   //updating groups
+                            }
+                          
+                            if (!visited[j])
+                            {
+                                cnt += 1;  //updating index of listViewScanned.Items
+                                imageListScanned.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(listFiles[j]));
+                                listViewScanned.Items.Add(fi2.Name,cnt);
+                                listViewScanned.Items[cnt].Checked = true;
+                                visited[j] = true;
+                            }
+                        }
+                    }
+                }
+                progressBar.Value += 1;
+                textBoxGroup.Text = Convert.ToString(groups); //Displaying total groups identified
+                if (groups == listFiles.Count)
+                {
+                    textBoxStatus.Text = "No duplicates found!";
+                }
+                else
+                {
+                    textBoxStatus.Text = "Duplicates found!";
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            progressBar.Minimum = 0;        //initialize progresbar minimum value
+            progressBar.Maximum = listFiles.Count;    //initialize progress bar maximum value
+            progressBar.Value = 0;   //starting value of progress bar
+            if (groups > listFiles.Count)
+            {
+                for (int i = 0; i < listFiles.Count; i++)
+                {
+                    progressBar.Value = i;
+                    string path = Path.Combine(txtPath.Text, listViewScanned.Items[i].Text);
+                    if (listViewScanned.Items[i].Checked == true)
+                    {
+                        File.Delete(path);
+                    }
+                }
+                progressBar.Value += 1;
+                textBoxStatus.Text = "Done!";
+            }
+            else
+            {
+                progressBar.Value = listFiles.Count;
+                textBoxStatus.Text = "Nothing To Do!";
+            }          
+        }
+
+        private void linkLabelPrj_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/raviverma2791747/Duplicate-Phoaster");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            operation = false;
+        }
+    }
+}
